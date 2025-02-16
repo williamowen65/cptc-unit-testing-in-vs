1759905df6248d066058eb3292ac9eeb7850ceda
William Owen <william.owen.bsc@gmail.com>
2025-02-16 13:53:26 -0800

    Add project files.

---
+++ cptc-unit-testing-in-vs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cptc_unit_testing_in_vs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
